80044fec007fa8ceb4bc9357b746195c9ba1a577
juma-classic <138252099+juma-classic@users.noreply.github.com>
2026-03-11 11:12:40 -0700

    Change website name from 'Tickshark' to 'State Fx'

--- src/utils/site-config.ts
+++ src/utils/site-config.ts
@@ -1,4 +1,4 @@
 export const website_domain = 'tickshark.top';
-export const website_name = 'Tickshark';
+export const website_name = 'State Fx';
 export const default_title = website_name;
 export const TRACKING_STATUS_KEY = 'tracking_status';
